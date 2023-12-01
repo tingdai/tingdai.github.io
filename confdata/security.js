3c330aee5069347f44030825f824d4bb8612883c
tingdai <ting.dai@ibm.com>
2023-12-01 13:59:44 -0500

    trying to add a world map

--- confdata/security.js
+++ confdata/security.js
@@ -1,3 +1,3 @@
 var security = [
-    {"abbrv": "security", "deadline": {"Summer Submission": "2023-06-06 23:59:59", "Fall Submission": "2023-10-17 23:59:59", "Winter Submission": "2023-12-08 23:59:59"}, "timezone": "AoE", "period": "August 14-16 2024", "place": "Philadelphia, PA", "link": "https://www.usenix.org/conference/usenixsecurity24"},
+    {"abbrv": "security", "deadline": {"1st Cycle Submission": "2023-06-06 23:59:59", "2nd Cycle Submission": "2023-10-17 23:59:59", "3rd Cycle Submission": "2023-12-08 23:59:59"}, "timezone": "AoE", "period": "August 14-16 2024", "place": "Philadelphia, PA", "link": "https://www.usenix.org/conference/usenixsecurity24"},
 ]
