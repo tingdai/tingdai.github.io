c2b0d5cae3569f75a3de64ec9229e845539d677e
tingdai <ting.dai@ibm.com>
2024-02-09 14:39:41 -0500

    update

--- confdata/icse.js
+++ confdata/icse.js
@@ -1,4 +1,5 @@
 var icse = [
-        {"abbrv": "icse", "deadline": {"1st Cycle Submission": "2023-05-29 23:59:59", "2nd Cycle Submission": "2023-08-01 23:59:59", "NIER Submission": "2023-09-14", "SEIP,SEIS Submission": "2023-10-06"}, "timezone": "AoE", "period": "April 14-20, 2024", "place": "Lisbon", "link": "https://conf.researchr.org/home/icse-2024"},
+        {"abbrv": "icse", "deadline": {"1st Cycle Submission": "2024-03-22 23:59:59", "2nd Cycle Submission": "2024-08-02 23:59:59", "NIER Submission": "2024-10-10", "SEIP,SEIS Submission": "2024-10-10"}, "timezone": "AoE", "period": "April 26-May 4, 2025", "place": "Ottawa, Ontario, Canada", "link": "https://conf.researchr.org/home/icse-2025"},
+        {"abbrv": "icse", "deadline": {"1st Cycle Submission": "2023-05-29 23:59:59", "2nd Cycle Submission": "2023-08-01 23:59:59", "NIER Submission": "2023-09-14", "SEIP,SEIS Submission": "2023-10-06"}, "timezone": "AoE", "period": "April 14-20, 2024", "place": "Lisbon, Portugal", "link": "https://conf.researchr.org/home/icse-2024"},
         {"abbrv": "icse", "deadline": {"Technical Submission": "2020-08-28 23:59:59", "Other Tracks Submission": "2020-10-18 23:59:59"}, "timezone": "AoE", "period": "May 23-29, 2021", "place": "Madrid, Spain", "link": "https://conf.researchr.org/home/icse-2021"},
 ]
