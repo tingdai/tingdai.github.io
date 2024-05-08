4689d74af6313e13988b88f5277504e8e2408b2f
tingdai <ting.dai@ibm.com>
2024-05-08 10:40:19 -0400

    sosp goes annual

--- confdata/sosp.js
+++ confdata/sosp.js
@@ -1,4 +1,5 @@
 var sosp = [
+    {"abbrv": "sosp", "deadline": {"Submission": "2024-04-19 23:59:59"}, "timezone": "PT", "period": "November 4–6, 2024", "place": "Austin, Texas, USA", "link": "https://sigops.org/s/conferences/sosp/2024/index.html"},
     {"abbrv": "sosp", "deadline": {"Submission": "2023-04-17 23:59:59"}, "timezone": "ET", "period": "October 23-26, 2023", "place": "Koblenz, Germany", "link": "https://sosp2023.mpi-sws.org/"},
     {"abbrv": "sosp", "deadline": {"Abstract": "2021-04-30 14:59:59", "Submission": "2021-05-07 14:59:59"}, "timezone": "PST", "period": "October 25-28, 2021", "place": "Koblenz, Germany", "link": "https://sosp2021.mpi-sws.org/"},
 ]
